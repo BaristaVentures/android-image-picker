e2dfba539946cd2c9fa051f57dd30eebb7b238b4
caipivara <danielgomezrico@gmail.com>
2019-05-23 11:16:26 -0500

    Fix cp env script replaces local gradle.properties

--- scripts/cp-env-to-properties.sh
+++ scripts/cp-env-to-properties.sh
@@ -10,20 +10,12 @@
 set +x // Hide all output
 
 ENV_VARIABLES=$(printenv | tr ' ' '\n')
-IFS=""
 
-# Decode base64 file
-echo $DA_SIGN_SECRET_RING_FILE | base64 --decode --ignore-garbage >> secring.gpg
-cp secring.gpg lib/secring.gpg
-cp secring.gpg reporter-pivotal/secring.gpg
-
-for PROPERTIES_FILE in gradle.properties lib/bintray.properties reporter-pivotal/bintray.properties
+for PROPERTIES_FILE in gradle.properties lib/bintray.properties
 do
   # Decode variables named like 'sign_BARISTA_DOT_password=123' to 'sign.password=123'.
   # Some ci servers like circle-ci don't support env variable names with a '.'
-  echo ${ENV_VARIABLES//_BARISTA_DOT_/.} > $PROPERTIES_FILE
-
-  echo signing.secretKeyRingFile=secring.gpg >> $PROPERTIES_FILE
+  echo ${ENV_VARIABLES//_BARISTA_DOT_/.} >> $PROPERTIES_FILE
 done
 
 set -x
